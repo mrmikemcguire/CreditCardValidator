e9d9de2e3509c2628d8f2846e64ab47f6f84a9cd
mrmikemcguire <mcguire@mullenhigh.com>
2014-10-27 08:41:54 -0600

    Trying out string approach

--- Credit Card Validator/src/CreditCardValidator.java
+++ Credit Card Validator/src/CreditCardValidator.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CreditCardValidator
 	{
-	static int cardNumber;
+	static String cardNumber;
+	static ArrayList <String> numberArray = new ArrayList <String>();
 	
 	public static void main(String[] args)
 		{
 		acceptCCNumber();
-		validateCCNumber();
+		loadArray();
+		//validateCCNumber();
 		}
 	
-	public static int acceptCCNumber()
+	public static String acceptCCNumber()
 		{
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please input the credit card number without spaces.");
-		cardNumber = userInput.nextInt();
+		cardNumber = userInput.nextLine();
 		return cardNumber;
+		}
+	
+	public static void loadArray()
+		{
+		for (int i = 0; i < cardNumber.length(); i++)
+			{
+			numberArray.add(cardNumber.substring(i, i + 1));
+			}
+		
+		
+		for(String fred : numberArray)
+			{
+			System.out.println(fred);
+			}
 		}
 	
 	public static void validateCCNumber()
