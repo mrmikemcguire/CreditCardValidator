fcb02a601385a32fedfb1ca15c24c33c2605e39f
mrmikemcguire <mcguire@mullenhigh.com>
2014-10-27 10:20:19 -0600

    Done!

--- Credit Card Validator/src/CreditCardValidator.java
+++ Credit Card Validator/src/CreditCardValidator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CreditCardValidator
 	{
-	static long cardNumber;
+	static long cardNumber, tempCardNumber;
 	static boolean validity;
 	static long [ ] rawNumbers = new long [16];
 	static long [ ] numbersToAdd = new long [16];
@@ -21,21 +21,16 @@ public class CreditCardValidator
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please input the credit card number without spaces.");
 		cardNumber = userInput.nextLong();
-		System.out.println("Your credit card # is " + cardNumber);
 		return cardNumber;
 		}
 	
 	public static void loadArray()
 		{
+		tempCardNumber = cardNumber;
 		for (int i = 0; i < 16; i++)
 			{
-			rawNumbers[i] = cardNumber % 10;
-			cardNumber = cardNumber / 10;
-			}
-		System.out.println("The 1st loaded array:");
-		for(long fred : rawNumbers)
-			{
-			System.out.print(fred);
+			rawNumbers[i] = tempCardNumber % 10;
+			tempCardNumber = tempCardNumber / 10;
 			}
 		}
 	
@@ -55,21 +50,12 @@ public class CreditCardValidator
 					}
 				else
 					{
-					System.out.println("Raw number is " + rawNumbers[i]);
 					long doubledNumber = rawNumbers[i] * 2;
-					System.out.println("Doubled number is " + doubledNumber);
 					long onesDigit = doubledNumber % 10;
-					System.out.println("One's digit is " + onesDigit);
 					long tensDigit = doubledNumber / 10;
-					System.out.println("Ten's digit is " + tensDigit);
-					System.out.println("Replaced number is " + (onesDigit + tensDigit));
 					numbersToAdd[i] = (onesDigit + tensDigit);
 					}
 				}
-			}
-		for(long bambam : numbersToAdd)
-			{
-			System.out.println(bambam);
 			}
 		}
 	
@@ -80,6 +66,9 @@ public class CreditCardValidator
 			{
 			total += wilma;
 			}
+		System.out.println();
+		System.out.println("The sum of all digits is " + total);
+		
 		if (total % 10 == 0)
 			{
 			validity = true;
@@ -89,7 +78,7 @@ public class CreditCardValidator
 	
 	public static void displayResult()
 		{
-		System.out.print("The credit card number " + cardNumber + " is ");
+		System.out.println("The credit card number " + cardNumber + " is ");
 		if (validity == true)
 			{
 			System.out.print("potentially valid.");
@@ -98,7 +87,5 @@ public class CreditCardValidator
 			{
 			System.out.print("invalid.");
 			}
-		}
-	
-	
+		}		
 	}
